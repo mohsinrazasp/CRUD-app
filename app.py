188fa991387b0ede7f3e97fb8858672ee140b8f1
mohsinrazasp <mohsinrazasp130@gmail.com>
2025-11-19 14:45:56 +0500

    working code

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
-from flask_socketio import SocketIO, emit
-
+from flask import Flask, redirect ,render_template, request
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
 
+tasks=[]
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
-app.run(app, port=5001)
+def home():
+    return render_template('index.html', tasks=tasks)
+
+
+@app.route('/add')
+def show_add_form():
+    return render_template('add.html')
+
+
+@app.route ('/add',methods=['POST'])
+def add_task():
+
+    task_name = request.form['task_name']
+    tasks.append(task_name)
+    return redirect('/')
+
+
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+
+    tasks.pop(task_id)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
